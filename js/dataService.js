188a7bceaaeca3befd7f3b75b0945765e2dc08ee
David Thompson <matkatdavid@gmail.com>
2021-11-11 21:59:02 -0700

    testing write rules

--- js/dataService.js
+++ js/dataService.js
@@ -1,41 +1,62 @@
 function convertToJson(response) {
-    if (response.ok) {
-        return response.json();
-    } else {
-        throw new Error("Bad response!");
-    }
+  if (response.ok) {
+    return response.json();
+  } else {
+    throw new Error("Bad response!");
+  }
 }
 
-const baseUrl = "https://precipice-ponderings-default-rtdb.firebaseio.com/"
+const baseUrl = "https://precipice-ponderings-default-rtdb.firebaseio.com/";
 
 export default class DataService {
-    constructor() {    }
-    /** Gets content from json file */
-    getContent(url) {
-        return fetch(url)
-            .then(convertToJson)
-            .then((data) => {
-                console.log(data);
-                return data;            
-            });
-    } 
+  constructor() {}
+  /** Gets content from json file */
+  getContent(url) {
+    return fetch(url)
+      .then(convertToJson)
+      .then((data) => {
+        console.log(data);
+        return data;
+      });
+  }
 
-    /** Returns data filtered by category of review */
-    async getByCategory(url, category) {
-        const content = await this.getContent(baseUrl + url + `?orderBy="category"&equalTo="${category}"`);
-        //const filtered = content.filter((item) => item.category === category);
-        return [...Object.values(content)];
-    }
-    /** Returns data attached to object with given slug */
-    async getBySlug(url, slug) {
-        const content = await this.getContent(baseUrl + url);
-        const find = content.find((item) => item.slug === slug);
-        return find;
-    }
+  async postCategory(url) {
+    // Checking to see if firebase write rules are secure
+    let options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        posting: {
+          img: "images/hikingPano_2500.jpg",
+          alt: "Attempt to post",
+          description: "If rules are right, this should be rejected",
+        },
+      }),
+    };
+    const content = await fetch(baseUrl + url, options).then(convertToJson);
+    console.log(content);
+  }
 
-    /** Returns object with given category */
-    async getCategoryDetails(url, category) {
-        const content = await this.getContent(baseUrl + url);
-        return content[category];
-    }
-};
+  /** Returns data filtered by category of review */
+  async getByCategory(url, category) {
+    const content = await this.getContent(
+      baseUrl + url + `?orderBy="category"&equalTo="${category}"`
+    );
+    //const filtered = content.filter((item) => item.category === category);
+    return [...Object.values(content)];
+  }
+  /** Returns data attached to object with given slug */
+  async getBySlug(url, slug) {
+    const content = await this.getContent(baseUrl + url);
+    const find = content.find((item) => item.slug === slug);
+    return find;
+  }
+
+  /** Returns object with given category */
+  async getCategoryDetails(url, category) {
+    const content = await this.getContent(baseUrl + url);
+    return content[category];
+  }
+}
--- js/landing.js
+++ js/landing.js
@@ -1,21 +1,21 @@
 import DataService from "./dataService.js";
-import utils from "./utils.js"
-const myDataService = new DataService;
+import utils from "./utils.js";
+const myDataService = new DataService();
 
 /** Dynamically builds a review landing page */
 function buildPage(data, category, categoryData) {
-    document.querySelector("#activityName").textContent = category;
-    document.querySelector(".description").textContent = categoryData.description;
-    document.querySelector("#landingHero").src = categoryData.img;
-    document.querySelector("#landingHero").alt = categoryData.alt;
-    const section = document.querySelector("#thumbnailGrid");
+  document.querySelector("#activityName").textContent = category;
+  document.querySelector(".description").textContent = categoryData.description;
+  document.querySelector("#landingHero").src = categoryData.img;
+  document.querySelector("#landingHero").alt = categoryData.alt;
+  const section = document.querySelector("#thumbnailGrid");
 
-    section.innerHTML += data.map((e) => buildThumbnail(e)).join("");
+  section.innerHTML += data.map((e) => buildThumbnail(e)).join("");
 }
 
 /** Builds a thumbnail for every review in post.json */
 function buildThumbnail(data) {
-    return `<div class="thumbnail">
+  return `<div class="thumbnail">
     <a href="review.html?slug=${data.slug}">
       <img src="${data.image}" alt="${data.alt}"/>
       <div>
@@ -23,13 +23,20 @@ function buildThumbnail(data) {
       <p>${data.date}</p>
       </div>
     </a>
-  </div>`
+  </div>`;
 }
 
-window.addEventListener("load", async function() {
-    const category = utils.getParams("category");
+window.addEventListener("load", async function () {
+  const category = utils.getParams("category");
 
-    const allArticles = await myDataService.getByCategory("content.json", category);
-    const aCategory = await myDataService.getCategoryDetails("categories.json", category);
-    buildPage(allArticles, category, aCategory);
+  const allArticles = await myDataService.getByCategory(
+    "content.json",
+    category
+  );
+  const aCategory = await myDataService.getCategoryDetails(
+    "categories.json",
+    category
+  );
+  // myDataService.postCategory("categories.json");
+  buildPage(allArticles, category, aCategory);
 });
